793e3bd2c8488957eb373db6be6b2e2c69aab614
nicowxdvd <nicowxdvd@gmail.com>
2020-03-27 22:36:29 -0300

    Game in Unity - COLLISION
    movement

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour{
+
+    public  PlayerMovement movement;
+
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        Debug.Log(collisionInfo.collider.name);
+        if ( collisionInfo.collider.tag == "Obstacle" ) {
+            Debug.Log("We hit an " + collisionInfo.collider.name);
+            movement.enabled = false;
+        }
+    }
+
+
+
+}
